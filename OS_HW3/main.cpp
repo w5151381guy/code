1c882e5397303402567a4bb1dd13379d671e4d1b
macbook <w5151381guy@gmail.com>
2016-06-25 16:17:01 +0800

    complete main function

--- OS_HW3/main.cpp
+++ OS_HW3/main.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
-int main()
+void OPT(vector<string> str){
+
+}
+void LRU(vector<string> str){
+
+}
+int main(int argc,char *argv[]){
+  string input;
+  vector<string> str;        //vector string for input
+  int length,frame;          //length = reference string length
+  cin >> length >> frame;
+  for(int i = 0 ; i < length ; i++){
+    cin >> input;
+    str.push_back(input);
+  }
+  if(argv[1] == 0)
+    OPT(str);
+  else
+    LRU(str);
+  return 0;
+}

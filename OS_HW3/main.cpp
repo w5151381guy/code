fa1503a4716221494c8e406fbc6201601a54fe90
macbook <w5151381guy@gmail.com>
2016-06-25 22:09:16 +0800

    finish LRU

--- OS_HW3/main.cpp
+++ OS_HW3/main.cpp
@@ -40,18 +40,50 @@ void OPT(vector<string> str,int frame){
         str1[p] = str[i];
       }
     }
-    sort(str1.begin(),str1.end());
     vec.push_back(str1);
   }
   cout << counter << endl;
   for(int i = 0 ; i < vec.size() ; i++){
+    sort(vec[i].begin(),vec[i].end());
     for(int j = vec[i].size() - 1 ; j >= 0 ; j--)
-      cout << vec[i][j];
+      cout << vec[i][j] << " ";
     cout << endl;
   }
 }
 void LRU(vector<string> str,int frame){
-
+  vector<string> str1;              //vector to compare every page
+  vector<vector<string> > vec;      //vector for print every page
+  int counter = 0;                  //counter for page fault
+  str1.push_back(str[0]);
+  vec.push_back(str1);
+  counter++;
+  for(int i = 1 ; i < str.size() ; i++){
+    bool check = false;
+    for(int j = 0 ; j < str1.size() ; j++){
+      if(str[i] == str1[j]){
+        check = true;
+        str1.insert(str1.end(),str[i]);
+        str1.erase(str1.begin() + j);
+      }
+    }
+    if(check == false){
+      counter++;
+      if(str1.size() < frame)
+        str1.push_back(str[i]);
+      else{
+        str1.erase(str1.begin());
+        str1.push_back(str[i]);
+      }
+    }
+    vec.push_back(str1);
+  }
+  cout << counter << endl;
+  for(int i = 0 ; i < vec.size() ; i++){
+    sort(vec[i].begin(),vec[i].end());
+    for(int j = vec[i].size() - 1 ; j >= 0 ; j--)
+      cout << vec[i][j] << " ";
+    cout << endl;
+  }
 }
 int main(int argc,char *argv[]){
   string input;

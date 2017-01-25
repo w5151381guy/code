68eff6eb07f72823255a011ea708d001ec82c04e
macbook <w5151381guy@gmail.com>
2017-01-25 16:52:31 +0800

    add some function

--- C#/bmi.cs
+++ C#/bmi.cs
@@ -1,18 +1,43 @@
 using System;
 class bmi{
-  public void cal(){
+  public double bmiCal(){
     string height,weight;
+	Console.Write("Please input your height(cm): ");
     height = Console.ReadLine();
+	Console.Write("Please input your weight(kg): ");
     weight = Console.ReadLine();
-    double HEIGHT,WEIGHT,ans;
-    HEIGHT = Convert.ToDouble(height);
-    WEIGHT = Convert.ToDouble(weight);
-    ans = WEIGHT / Math.Pow(HEIGHT/100,2);
-    Console.Write("Your BMI is: ");
-    Console.Write(ans);
+    double Height,Weight,ans;
+    Height = Convert.ToDouble(height);
+    Weight = Convert.ToDouble(weight);
+    ans = Math.Round(Weight / Math.Pow(Height/100,2),2);
+	return ans;
+  }
+  public void print(double bmiValue,int evaluateValue){
+	Console.Write("Your BMI is: ");
+	Console.WriteLine(bmiValue);
+	switch(evaluateValue){
+		case 1:Console.WriteLine("You should eat more!!!");break;
+		case 2:Console.WriteLine("Nice! Keep maintaining!");break;
+		case 3:Console.WriteLine("You should eat less!!!");break;
+	}
+  }
+  public int evaluate(double bmiValue){
+	/*1 -> thin ; 2 -> normal ; 3 -> heavy*/
+	if(bmiValue < 18.5)
+		return 1;
+	else{
+		if(bmiValue < 24)
+			return 2;
+		else
+			return 3;
+	}
   }
   static void Main(){
     bmi b = new bmi();
-    b.cal();
+	double bmiValue;
+    bmiValue = b.bmiCal();
+	int evaluateValue;
+	evaluateValue = b.evaluate(bmiValue);
+	b.print(bmiValue,evaluateValue);
   }
 }

a2e8dfc1b682cd845b5d44115dd7dbedf69bb972
macbook <w5151381guy@gmail.com>
2017-01-25 16:21:31 +0800

    modify

--- C#/bmi.cs
+++ C#/bmi.cs
@@ -2,14 +2,14 @@ using System;
 class bmi{
   public void cal(){
     string height,weight;
-    height = System.Console.ReadLine();
-    weight = System.Console.ReadLine();
+    height = Console.ReadLine();
+    weight = Console.ReadLine();
     double HEIGHT,WEIGHT,ans;
     HEIGHT = Convert.ToDouble(height);
     WEIGHT = Convert.ToDouble(weight);
     ans = WEIGHT / Math.Pow(HEIGHT/100,2);
-    System.Console.Write("Your BMI is: ");
-    System.Console.Write(ans);
+    Console.Write("Your BMI is: ");
+    Console.Write(ans);
   }
   static void Main(){
     bmi b = new bmi();

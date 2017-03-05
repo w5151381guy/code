b2f092726c35662e4ad9d1a12f4d1eb9a5237076
macbook <w5151381guy@gmail.com>
2017-03-05 16:33:12 +0800

    finish homework 1

--- opencv_python/test.py
+++ opencv_python/test.py
@@ -1,10 +1,10 @@
 import numpy as np
 import cv2
 
-img1 = cv2.imread('YZU.jpg',1)   #readimage in true color
-img2 = cv2.imread('YZU.jpg',0)   #readimage in grayscale
+img1 = cv2.imread('lena.png',1)   #readimage in RGB
+img2 = cv2.imread('lena.png',0)   #readimage in grayscale
 
-#display image in true color
+#display image in RGB
 cv2.imshow('image1',img1)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
@@ -12,3 +12,38 @@ cv2.destroyAllWindows()
 #display image in grayscale
 cv2.imshow('image2',img2)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#user input
+value = int(input("Please input value to add or substract: "))
+
+#catch image properities
+col,row,pix = img1.shape 
+height,weight = img2.shape
+
+#change every pixel in picture
+for i in range(0,col,+1):
+    for j in range(0,row,+1):
+        for k in range(0,pix,+1):
+            if img1[i,j,k] + value <= 255:
+                img1[i,j,k] += value
+            else:
+                img1[i,j,k] = 255
+for i in range(0,height,+1):
+    for j in range(0,weight,+1):
+        if img2[i,j] + value <= 255:
+            img2[i,j] += value
+        else:
+            img2[i,j] = 0
+
+#display after calculate
+cv2.imshow('After_image1',img1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imshow('After_image2',img2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imwrite('After_image1.jpg',img1)
+cv2.imwrite('After_image2.jpg',img2)

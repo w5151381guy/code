75a4e8870c15555151dcccfd516b98c1affff009
macbook <w5151381guy@gmail.com>
2017-03-05 14:54:47 +0800

    read image

--- opencv_python/test.py
+++ opencv_python/test.py
@@ -1,4 +1,14 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('YZU_logo.png',1)
+img1 = cv2.imread('YZU.jpg',1)   #readimage in true color
+img2 = cv2.imread('YZU.jpg',0)   #readimage in grayscale
+
+#display image in true color
+cv2.imshow('image1',img1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#display image in grayscale
+cv2.imshow('image2',img2)
+cv2.waitKey(0)

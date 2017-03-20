e6bd15accb6b1ea27df51e8a36a7ccb65b386de3
macbook <w5151381guy@gmail.com>
2017-03-20 14:16:07 +0800

    add comment

--- opencv_python/image_scale.py
+++ opencv_python/image_scale.py
@@ -1,10 +1,11 @@
 import numpy as np
 import cv2
-
+#read image
 img = cv2.imread('lena.png',1) #RGB
 cv2.imshow('original_image',img)
 cv2.waitKey(0)
 
+#rotate image
 degree = int(input("Please input degree to rotate image: "))
 row,col,pix = img.shape
 M = cv2.getRotationMatrix2D((col/2,row/2),degree,1)
@@ -12,8 +13,8 @@ rotate = cv2.warpAffine(img,M,(col,row))
 cv2.imshow('after_rotate',rotate)
 cv2.waitKey(0)
 
+#image scaling
 scale = cv2.resize(img, None, fx = 1.2, fy = 1.2, interpolation = cv2.INTER_CUBIC)
-
 cv2.imshow('after_scaling',scale)
 cv2.waitKey(0)
 cv2.destroyAllWindows()

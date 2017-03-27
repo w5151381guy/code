e06f5f9ccf814e34045a323ea980d4e96de74fa7
macbook <w5151381guy@gmail.com>
2017-03-27 15:51:00 +0800

    finish

--- opencv_python/image_histogram.py
+++ opencv_python/image_histogram.py
@@ -5,6 +5,7 @@ from matplotlib import pyplot as plt
 img = cv2.imread('lena.png',0)
 hist,bins = np.histogram(img.flatten(),256,[0,256])
 
+#numpy implementation
 cdf = hist.cumsum()
 cdf_normalized = cdf * hist.max() / cdf.max()
 plt.plot(cdf_normalized, color = 'b')
@@ -12,3 +13,10 @@ plt.hist(img.flatten(),256,[0,256],color = 'r')
 plt.xlim([0,256])
 plt.legend(('cdf','histogram'),loc = 'upper left')
 plt.show()
+
+#histogram equalization
+equ = cv2.equalizeHist(img)
+dst = np.hstack((img,equ))
+cv2.imshow('histogram_equalization',dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

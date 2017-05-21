eec622a8bb8d0e0d599b91f0f5841611c8f2ad02
macbook <w5151381guy@gmail.com>
2017-05-21 15:59:55 +0800

    finish filter

---
+++ opencv_python/filter.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+import math
+
+def GaussianFilter(length,weight):
+    Kernel = np.zeros((length,length))
+    total = 0
+
+    KernelRadius = length / 2
+    distance = 0
+
+    Euler = 1.0 / (2.0 * math.pi * weight * weight)
+
+    number = range(-KernelRadius,KernelRadius+1)
+
+    for i in number:
+        for j in number:
+            distance = ((j * j) + (i * i)) / (2 * weight * weight)
+            Kernel[j+KernelRadius,i+KernelRadius] = Euler * math.exp(-distance)
+            total += Kernel[j+KernelRadius,i+KernelRadius]
+    
+    for i in range(length):
+        for j in range(length):
+            Kernel[i,j] = Kernel[i,j] * (1/total)
+
+    return Kernel
+
+if __name__ == '__main__':
+    img = cv2.imread('lena.png',0)
+    K = GaussianFilter(7,0.84089642)
+    #print(K)
+    
